534fe7a4137740b4f436ed47723e5ae293a0e5e6
Nikita2993-Z <zinchenko.nikita93@gmail.com>
2025-03-29 12:21:45 +0000

    Deploying to gh-pages from @ Nikita2993-Z/goit-js-hw-10@064f1da84254aa4649ed8937781ab6da40eeb97c 🚀

---
+++ index.js
@@ -0,0 +1,2 @@
+import{i as t}from"./assets/1-timer-0HLKzWA6.js";/* empty css                      */import"./assets/vendor-BbSUbo7J.js";document.addEventListener("DOMContentLoaded",()=>{t()});
+//# sourceMappingURL=index.js.map
